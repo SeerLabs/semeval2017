8471a15d1b443d1624749e3eea8d65401436046b
Agnese Chiatti <agnese.chiatti@gmail.com>
2016-10-11 21:03:10 -0400

    Update crfsuite.py

--- crfsuite.py
+++ crfsuite.py
@@ -1,2 +1,10 @@
-import crfsuite
+**********************
+Terry Peng <pengtaoo@gmail.com>
+Mikhail Korobov <kmike84@gmail.com>
+Bundled CRFSuite C/C++ library is by Naoaki Okazaki & contributors.
+
+
+import pycrfsuite
+
+
 
