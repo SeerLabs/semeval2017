b52dceea8e8469e5b69ae100fa03c5d6c24f7236
Agnese Chiatti <agnese.chiatti@gmail.com>
2016-10-11 21:17:02 -0400

    Update and rename crfsuite.py to pycrfsuite.py

---
+++ pycrfsuite.py
@@ -0,0 +1,12 @@
+# taking code from:
+#Terry Peng <pengtaoo@gmail.com>
+#Mikhail Korobov <kmike84@gmail.com>
+#Bundled CRFSuite C/C++ library is by Naoaki Okazaki & contributors.
+
+
+import pycrfsuite
+
+#training data provided for SemEval have to be adapted to this module
+
+
+
